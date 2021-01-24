1af21daedffa0a5689349ca60d404c78210b78ea
alberto <gutgonal@hotmail.es>
2021-01-24 12:29:21 +0100

    comentario

---
+++ jardin.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html>
+	<head>
+		<title>JARDIN</TITLE>
+	</head>
+	<body>
+		<CENTER>
+		<table  width="80%" weight="100%">
+			<tr>
+				<td bgcolor="00FFE2" >
+					<center>
+					<br><img src="img/jardin.jpg" width="300"></br>
+					</br>
+					Se quiere construir un jardín de "X" m de ancho alrededor de una fuente circular de "Y" m de diámetro.</br>
+					Si los paquetes de césped fuesen de "Z" m2, ¿cuántos paquetes harían falta comprar para cubrir el jardín?</br>
+					</br>					
+					<form>
+					Ancho del jardin<input id="x" name="x" onkeyup="calcular()" value="<?=$_GET['x']?>"/>
+					Diametro de fuente<input id="y" name="y" onkeyup="calcular()" value="<?=$_GET['y']?>"/>
+					Tamaño paquetes<input id="z" name="z" onkeyup="calcular()" value="<?=$_GET['z']?>"/>
+					</br>
+					</br><button>CALCULAR</button>
+					</br>
+					</br>
+					</form>
+					Area de jardin
+					<div id="resultado"></div>
+					numero de paquetes de cesped
+					<div id="resultado2"></div>
+					
+					
+					<script>
+						function calcular(){
+						x=document.getElementById('x').value;
+						y=document.getElementById('y').value;
+						z=document.getElementById('z').value;
+						x=parseFloat(x);
+						y=parseFloat(y);
+						z=parseFloat(z);
+						resultado=3.14*(x+(y/2))*(x+(y/2))*100/100;
+						resultado2= Math.round (3.14*(x+(y/2))*(x+(y/2))/z*100)/100;
+						document.getElementById('resultado').innerHTML=resultado;
+						document.getElementById('resultado2').innerHTML=resultado2;
+						
+						}
+					calcular()
+					
+					</script>
+					
+					<?php
+						$x=$_GET['x'];
+						$y=$_GET['y'];
+						$z=$_GET['z'];
+						echo area del jardin;
+						echo M_PI*($x+($y/2))*($x+($y/2)),2; 
+						echo numero de paquetes de cesped;
+						echo (M_PI*($x+($y/2))*($x+($y/2))/$z,2;
+					?>
+					
+					</center>
+				</td>
+			</tr>	
+			 
+		</table>
+		</CENTER>
+	</body>
+</html>
